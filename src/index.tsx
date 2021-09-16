a0b936316116fdcd6cb00d315b6139633d350f64
spgetter <spgetter@gmail.com>
2021-09-16 09:36:51 -0600

    day_3 commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,33 @@ import { Home, Dashboard, Signin } from './components';
 import './styles.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { FirebaseAppProvider, AuthCheck } from 'reactfire';
+import 'firebase/auth';
+import { firebaseConfig } from './firebaseConfig';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
+    <Provider store={ store }>
+      <Router>
+        <Switch>
 
-        <Route exact path='/'>
-          <Home title={"Thrones Inventory"} />
-        </Route>
+          <Route exact path='/'>
+            <Home title={"Thrones Inventory"} />
+          </Route>
 
-        <Route path='/dashboard'>
-          <Dashboard />
-        </Route>
+          <Route path='/dashboard'>
+            <Dashboard />
+          </Route>
 
-        <Route path='/signin'>
-          <Signin />
-        </Route>
+          <Route path='/signin'>
+            <Signin />
+          </Route>
 
-      </Switch>
-    </Router>
+        </Switch>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
